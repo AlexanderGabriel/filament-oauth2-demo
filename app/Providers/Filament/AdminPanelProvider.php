ca0d21ae6d492a4ce73289894aa7649a35a6b0eb
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2025-11-10 19:30:26 +0000

    first prototype, logout not working, not tested without roles

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use AlexanderGabriel\FilamentOauth2\FilamentOauth2Plugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -51,6 +52,9 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
+            ->plugin(
+                new FilamentOauth2Plugin()
+            )
             ->authMiddleware([
                 Authenticate::class,
             ]);
